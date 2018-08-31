54572987db8b71d79e565dfe075798214ec7b1ac
Michael McKinney <>
2018-08-31 17:37:22 -0400

    driver tests built

--- Root Insurance Code Test/RICTests/RICTests.swift
+++ Root Insurance Code Test/RICTests/RICTests.swift
@@ -32,25 +32,90 @@ class RICTests: XCTestCase {
 		
 	}
 	
-	func testDriverInitWithValues(){
-		
-	}
-	
 	func testDriverAddFirstTrip(){
+		let testDriver = Driver(Name: "mrTest")
 		
+		var testStartDateComponents = DateComponents()
+		testStartDateComponents.hour = 2
+		testStartDateComponents.minute = 30
+		
+		var testEndDateComponents = DateComponents()
+		testEndDateComponents.hour = 4
+		testEndDateComponents.minute = 30
+		
+		let testDistance:Float = 10.0
+		
+		testDriver.AddTrip(StartTimeComponents: testStartDateComponents, EndTimeComponents: testEndDateComponents, Distance: testDistance)
+		
+		//2 hours with 10 miles traveled
+		
+		XCTAssert(testDriver.Distance == 10)
+		XCTAssert(testDriver.Speed == 5)
 		
 	}
 	
 	func testDriverAddMultipleTrips(){
+		//First Trip
+		let testDriver = Driver(Name: "mrTest")
 		
+		var testStartDateComponents = DateComponents()
+		testStartDateComponents.hour = 2
+		testStartDateComponents.minute = 30
+		
+		var testEndDateComponents = DateComponents()
+		testEndDateComponents.hour = 4
+		testEndDateComponents.minute = 30
+		
+		var testDistance:Float = 10.0
+		
+		testDriver.AddTrip(StartTimeComponents: testStartDateComponents, EndTimeComponents: testEndDateComponents, Distance: testDistance)
+		
+		//2 hours with 10 miles traveled (5 mph)
+		
+		//Second Trip
+		testStartDateComponents.hour = 13
+		testStartDateComponents.minute = 45
+		
+		testEndDateComponents.hour = 18
+		testEndDateComponents.minute = 45
+		
+		testDistance = 50.0
+		
+		testDriver.AddTrip(StartTimeComponents: testStartDateComponents, EndTimeComponents: testEndDateComponents, Distance: testDistance)
+		
+		//5 hours with 50 miles traveled (10 mph)
+		
+		XCTAssert(testDriver.Distance == 60)
+		XCTAssert(testDriver.Speed == 9)
 	}
 	
 	func testDriverSummaryForNoMiles(){
+		let testDriver = Driver(Name: "mrTest")
+		let mrTestsString = testDriver.ResultsSummary()
 		
+		XCTAssert(mrTestsString == "mrTest: 0 miles")
 	}
 	
 	func testDriverSummaryForSomeMiles(){
+		let testDriver = Driver(Name: "mrTest")
 		
+		var testStartDateComponents = DateComponents()
+		testStartDateComponents.hour = 2
+		testStartDateComponents.minute = 30
+		
+		var testEndDateComponents = DateComponents()
+		testEndDateComponents.hour = 4
+		testEndDateComponents.minute = 30
+		
+		var testDistance:Float = 10.0
+		
+		testDriver.AddTrip(StartTimeComponents: testStartDateComponents, EndTimeComponents: testEndDateComponents, Distance: testDistance)
+		
+		//2 hours with 10 miles traveled (5 mph)
+
+		let mrTestsString = testDriver.ResultsSummary()
+		
+		XCTAssert(mrTestsString == "mrTest: 10 miles @ 5mph")
 	}
 	
 	//MARK: VIEW MODEL UTILITY FUNCTIONS
